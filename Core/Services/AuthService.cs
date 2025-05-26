d55cdf315399ad18a9810a1649fdc7b1c48947bb
Antoine HEPH <antoine.goossens@condorcet.be>
2025-05-26 16:02:14 +0200

    réglage username lors de l'ajout

--- Core/Services/AuthService.cs
+++ Core/Services/AuthService.cs
@@ -62,7 +62,7 @@ public class AuthService: IAuthService
         var claims = new List<Claim>
         {
             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            new Claim(ClaimTypes.Name, user.FirstName),
+            new Claim(ClaimTypes.Name, user.Username),
             new Claim(ClaimTypes.Email, user.Email),
             new Claim(ClaimTypes.Role, user.Role)
         };
